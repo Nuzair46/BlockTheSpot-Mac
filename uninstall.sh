9792ffc2d129f362f8ba38ca7d9d9fbe8318d111
jetfir3 <95306468+jetfir3@users.noreply.github.com>
2022-10-01 12:44:11 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # Inital paths and filenames
-XPUI_PATH="/Applications/Spotify.app/Contents/Resources/Apps"
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     XPUI_PATH="/usr/share/spotify/Apps";;
+    Darwin*)    XPUI_PATH="/Applications/Spotify.app/Contents/Resources/Apps";;
+esac
 XPUI_SPA="xpui.spa"
 XPUI_SPA_BAK="xpui.bak"
 
