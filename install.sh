7c65331304867cc8df500dc1dc1836947c80858e
jetfir3 <95306468+jetfir3@users.noreply.github.com>
2022-10-02 17:26:01 -0400

    Add download option. Update uninstall script.

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 # Inital paths and filenames
+if [[ -d "~/Applications/Spotify.app" ]]; then
+    INSTALL_PATH="${HOME}/Applications/Spotify.app"
+elif [[ -d "/Applications/Spotify.app" ]]; then
+    INSTALL_PATH="/Applications/Spotify.app"
+else
+    INSTALL_PATH="/Applications/Spotify.app"
+    NOT_INSTALLED=yes
+fi
+UPDATER_PATH="${HOME}/Library/Application Support/Spotify/PersistentCache/Update"
 XPUI="xpui"
-XPUI_PATH="/Applications/Spotify.app/Contents/Resources/Apps"
+XPUI_PATH="${INSTALL_PATH}/Contents/Resources/Apps"
 XPUI_SPA="xpui.spa"
 XPUI_SPA_BAK="xpui.bak"
 XPUI_ZIP="xpui.zip"
@@ -23,16 +32,112 @@ PATCH_AUDIO_AD='case 0:;case 4:this.subscription=this.audioApi.cosmosConnector.i
 PATCH_BILLBOARD='(false?'
 
 # Credits
-echo "***************************************"
+echo "************************************************"
 echo "SpotX-Mac by @nuzair46"
 echo "Thanks to @amd64fox for the original SpotX patch"
-echo "***************************************"
+echo "************************************************"
+echo
+
+if [[ "$NOT_INSTALLED" == "yes" ]]; then
+    while true; do
+        read -n 1 -p 'Spotify.app not found, install Spotify? [Y/N] ' yn
+        case $yn in
+            [Yy]* )
+                DOWNLOAD_APP=yes
+                break;;
+            [Nn]* ) 
+                echo
+                echo -e "Exiting...\n"
+                exit
+                break;;
+                * ) echo -e "\nPlease enter yes or no.";;
+        esac
+    done
+else
+    while true; do
+        read -n 1 -p 'Do you want to download and install Spotify? [Y/N] ' yn
+        case $yn in
+            [Yy]* )
+                DOWNLOAD_APP=yes
+                break;;
+            [Nn]* ) 
+                DOWNLOAD_APP=no
+                break;;
+                * ) echo -e "\nPlease enter yes or no.";;
+        esac
+    done
+fi
+
+# Download and Install Spotify
+if [[ "$DOWNLOAD_APP" == "yes" ]]; then
+
+    # Detect Device Architecture
+    if [[ $(sysctl -n machdep.cpu.brand_string) =~ "Apple" ]]; then
+        buildarch=arm64
+        echo
+    else
+        buildarch=x86_64
+        echo
+    fi
+
+    # Build Number and Update Block Variables
+    read -p 'Enter desired build # (ex. 1.1.95.893.g6cf4d40c-39): ' buildno
+    while true; do
+        read -n 1 -p 'Do you want to block Spotify updates? [Y/N] ' yn
+        case $yn in
+            [Yy]* )
+                UPDATE_BLOCK=yes
+                break;;
+            [Nn]* ) 
+                UPDATE_BLOCK=no
+                break;;
+                * ) echo -e "\nPlease enter yes or no.";;
+        esac
+    done
+
+    # Download and Install
+    echo -e "\n\nDownloading...\n"
+    curl -f -o ${HOME}/Downloads/spotify-autoupdate-$buildno.tbz https://upgrade.scdn.co/upgrade/client/osx-$buildarch/spotify-autoupdate-$buildno.tbz && \
+    if [[ -d "${INSTALL_PATH}" ]]; then echo -e "\nDeleting current Spotify.app..." && osascript -e 'quit app "Spotify"' && rm -rf "${INSTALL_PATH}"; fi && \
+    echo "Installing..." && \
+    mkdir "${INSTALL_PATH}" && \
+    tar -xpjf ~/Downloads/spotify-autoupdate-$buildno.tbz -C "${INSTALL_PATH}" && \
+    echo "Cleaning up..." && rm ${HOME}/Downloads/spotify-autoupdate-$buildno.tbz
+else
+    # Update Block Variable
+    echo
+    while true; do
+        read -n 1 -p 'Do you want to block Spotify updates? [Y/N] ' yn
+        case $yn in
+            [Yy]* )
+                UPDATE_BLOCK=yes
+                echo
+                break;;
+            [Nn]* ) 
+                UPDATE_BLOCK=no
+                echo
+                break;;
+                * ) echo -e "\nPlease enter yes or no.";;
+        esac
+    done
+fi
+
+# Block Updates
+if [[ "$UPDATE_BLOCK" == "yes" ]]; then
+    echo "Blocking updates..."
+    chflags nouchg "${UPDATER_PATH}" 2> /dev/null
+    rm -rf "${UPDATER_PATH}"
+    mkdir -p "${UPDATER_PATH}"
+    chflags uchg "${UPDATER_PATH}"
+else
+    chflags nouchg "${UPDATER_PATH}" 2> /dev/null
+fi
 
 # Create backup and extract xpui.js
-echo "Creating backup of xpui.spa"
+echo "Creating backup of xpui.spa..."
 cp "$XPUI_PATH/$XPUI_SPA" "$XPUI_PATH/$XPUI_SPA_BAK"
 
-echo "Extracting xpui.js"
+echo "Extracting xpui.js..."
 cd "$XPUI_PATH"
 rm -rf "$XPUI"
 mv "$XPUI_SPA" "$XPUI_ZIP"
@@ -70,7 +175,7 @@ PATCH_BILLBOARD_AD=${MATCHED_STRING/$TO_PATCH/$PATCH_BILLBOARD}
 perl -pi -w -e "s/\Q$MATCHED_STRING\E/$PATCH_BILLBOARD_AD/g;" $XPUI_JS
 
 # Rebuild xpui.spa
-echo "Rebuilding xpui.spa"
+echo "Rebuilding xpui.spa..."
 
 #zip files inside xpui folder
 cd "$XPUI"
@@ -79,4 +184,4 @@ mv "$XPUI_SPA" "$XPUI_PATH"
 cd "$XPUI_PATH"
 rm -rf "$XPUI"
 
-echo "Patch applied successfully!"
+echo -e "Patch applied successfully!\n"
--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
 # Inital paths and filenames
-XPUI_PATH="/Applications/Spotify.app/Contents/Resources/Apps"
+if [[ -d "${HOME}/Applications/Spotify.app" ]]; then
+    XPUI_PATH="${HOME}/Applications/Spotify.app/Contents/Resources/Apps"
+elif [[ -d "/Applications/Spotify.app" ]]; then
+    XPUI_PATH="/Applications/Spotify.app/Contents/Resources/Apps"
+else
+    echo -e "Spotify.app not found.\nExiting...\n"
+    exit
+fi
 XPUI_SPA="xpui.spa"
 XPUI_SPA_BAK="xpui.bak"
+
+# Check for backup file
+if [[ ! -f "$XPUI_PATH/$XPUI_SPA_BAK"]]; then
+    echo -e "Backup file not found.\nExiting...\n"
+    exit
+fi
 
 # Uninstall patch
 echo "Removing patch..."
@@ -11,4 +24,4 @@ cd "$XPUI_PATH"
 rm "$XPUI_SPA"
 mv "$XPUI_SPA_BAK" "$XPUI_SPA"
 
-echo "Patch removed successfully!"
+echo -e "Patch removed successfully!\n"
