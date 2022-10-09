09250b3709190a99190864c8a29cd4c20b32d843
jetfir3 <95306468+jetfir3@users.noreply.github.com>
2022-10-09 14:07:59 -0400

    Update uninstall.sh
    - fix crash caused by line #18
    - minor cleanup and condensing

--- uninstall.sh
+++ uninstall.sh
@@ -3,27 +3,26 @@
 # Inital paths and filenames
 APP_PATH="/Applications/Spotify.app"
 if [[ -d "${HOME}${APP_PATH}" ]]; then
-    INSTALL_PATH="${HOME}${APP_PATH}"
+  INSTALL_PATH="${HOME}${APP_PATH}"
 elif [[ -d "${APP_PATH}" ]]; then
-    INSTALL_PATH="${APP_PATH}"
+  INSTALL_PATH="${APP_PATH}"
 else
-    echo -e "\nSpotify not found. Exiting...\n"
-    exit
+  echo -e "\nSpotify not found. Exiting...\n"
+  exit
 fi
 XPUI_PATH="${INSTALL_PATH}/Contents/Resources/Apps"
-XPUI_SPA="xpui.spa"
-XPUI_SPA_BAK="xpui.bak"
+XPUI_SPA="${XPUI_PATH}/xpui.spa"
+XPUI_BAK="${XPUI_PATH}/xpui.bak"
 
 # Check for backup file
-if [[ ! -f "$XPUI_PATH/$XPUI_SPA_BAK"]]; then
-    echo -e "Backup file not found.\nExiting...\n"
-    exit
+if [[ ! -f "${XPUI_BAK}" ]]; then
+  echo -e "Backup file not found.\nExiting...\n"
+  exit 
 fi
 
 # Uninstall patch
 echo "Removing patch..."
-cd "$XPUI_PATH"
-rm "$XPUI_SPA"
-mv "$XPUI_SPA_BAK" "$XPUI_SPA"
+rm "${XPUI_SPA}"
+mv "${XPUI_BAK}" "${XPUI_SPA}"
 
 echo -e "Patch removed successfully!\n"
